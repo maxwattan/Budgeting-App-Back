32ad086a21186e4b6156a76d921258d0e65497a7
MaxWattan <mcwattanac@gmail.com>
2023-04-21 23:59:01 -0400

    added more data

--- Backend/models/transactions.js
+++ Backend/models/transactions.js
@@ -2,7 +2,7 @@ module.exports = [
     {
     id: 1,
     item_name: "Pizza",
-    amount: 20.00,
+    amount: -20.00,
     date: "4/17/23",
     from: "roberta",
     category: "food",
@@ -10,10 +10,60 @@ module.exports = [
 },
 {
     id: 2,
-    item_name: "coffee",
-    amount: 10.00,
+    item_name: "",
+    amount: 2500.00,
+    date: "4/19/23",
+    from: "income",
+    category: "uber",
+},
+{
+    id: 3,
+    item_name: "movie ticket",
+    amount: -15.00,
     date: "4/18/23",
-    from: "starbucks",
+    from: "amc",
+    category: "entertainment",
+},
+{
+    id: 4,
+    item_name: "",
+    amount: -120.00,
+    date: "4/15/23",
+    from: "chase",
+    category: "bill",
+},
+{
+    id: 5,
+    item_name: "burger",
+    amount: -20.00,
+    date: "4/10/23",
+    from: "shack shack",
     category: "food",
-}
+},
+{
+    id: 6,
+    item_name: "",
+    amount: -20.00,
+    date: "3/27/23",
+    from: "planet fitness",
+    category: "gym",
+},
+{
+    id: 7,
+    item_name: "sandwhich",
+    amount: -14.00,
+    date: "3/22/23",
+    from: "panera",
+    category: "food",
+},
+{
+    id: 8,
+    item_name: "",
+    amount: 2400,
+    date: "3/19/23",
+    from: "uber",
+    category: "income",
+},
+
+
 ]
